c78d1eb10f5bbb518189a2cc4fbc5f228de40416
david <david@airbud.io>
2023-03-02 18:07:44 +0200

    add retry on rate limit from Google spreadsheet API

--- answers-database.ts
+++ answers-database.ts
@@ -1,4 +1,4 @@
-import { GoogleSpreadsheet } from 'google-spreadsheet'
+import { GoogleSpreadsheet } from "@noloco/google-spreadsheet"
 import creds from './google.json'
 
 const URL = process.env.ANSWERS || 'https://docs.google.com/spreadsheets/d/1Dk4qG6d7kjeK_iFbmxJ8Z2gGQRHZ_lsgdqwLiUxXMlg/edit#gid=0'
@@ -8,7 +8,8 @@ const googleRegex = /\/([\w-_]{15,})\/(.*?gid=(\d+))?/
 export const writeAnswers = async (data, url = URL) => {
   if (data.answers.length === 0) return url
   const doc = new GoogleSpreadsheet(googleRegex.exec(url)[1])
-  await doc.useServiceAccountAuth(JSON.parse(atob(creds['google'])))
+  await doc.useServiceAccountAuth(creds)
+  await doc.setRetryOptions(2, 60000)
   await doc.loadInfo()
 
   let existed = []
@@ -40,7 +41,8 @@ export const writeAnswers = async (data, url = URL) => {
 // Read quest answers from Google Spreadsheet
 export const readAnswers = async (url = URL) => {
   const doc = new GoogleSpreadsheet(googleRegex.exec(url)[1])
-  await doc.useServiceAccountAuth(JSON.parse(atob(creds['google'])))
+  await doc.useServiceAccountAuth(creds)
+  await doc.setRetryOptions(2, 60000)
   await doc.loadInfo()
 
   let answers = {}
@@ -60,7 +62,7 @@ const checkConnection = async () => {
     const answers = await readAnswers()
     console.log(`Google OK. Database communities: ${Object.keys(answers).join(' | ')}`)
   } catch (e) {
-    console.log(`Google connection error! Please check provided URL visability or use default URL.`)
+    console.log(`Google connection error! Please check provided URL visibility or use default URL.`)
   }
 }
 
